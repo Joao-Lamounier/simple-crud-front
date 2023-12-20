80d3d0a2c0a3e850598ffd918ba6e849f6f3289e
João Felipe Silva Lamounier <jjoaolamounier@gmail.com>
2023-12-20 19:33:21 -0300

    Iniciado listagem de pessoas

--- src/app/peoples/peoples.module.ts
+++ src/app/peoples/peoples.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PeoplesRoutingModule } from './peoples-routing.module';
-
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    PeoplesRoutingModule
-  ]
+  imports: [CommonModule, PeoplesRoutingModule],
+  exports: [MatTableModule]
 })
-export class PeoplesModule { }
+export class PeoplesModule {
+
+}
--- src/app/peoples/peoples/peoples.component.ts
+++ src/app/peoples/peoples/peoples.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { People } from '../model/people';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-peoples',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './peoples.component.html',
   styleUrls: ['./peoples.component.scss'],
 })
-export class PeoplesComponent {}
+export class PeoplesComponent {
+  peoples: People[] = [
+    {id: 1, nome: 'João Felipe', apelido: 'Jão', time: 'Cruzeiro', cpf: '08545360606', hobbie: 'estudar', cidade: {id: 1, nome: 'Camacho', estado: 'MG'}}
+  ];
+  displayedColumns = ['nome', 'apelido', 'time', 'cpf', 'hobbie', 'cidade'];
+}
