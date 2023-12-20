f579b8708472c96e0ac86d7ef263e7bab2ae0f38
João Felipe Silva Lamounier <jjoaolamounier@gmail.com>
2023-12-20 18:03:05 -0300

    Definida as paletas de cores

---
+++ src/app/peoples/peoples-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PeoplesComponent } from './peoples/peoples.component';
+
+const routes: Routes = [{ path: '', component: PeoplesComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PeoplesRoutingModule {}
---
+++ src/app/peoples/peoples/peoples.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-peoples',
+  standalone: true,
+  imports: [],
+  templateUrl: './peoples.component.html',
+  styleUrls: ['./peoples.component.scss'],
+})
+export class PeoplesComponent {}
